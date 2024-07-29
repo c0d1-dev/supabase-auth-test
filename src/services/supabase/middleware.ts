b29f363f846893dee46dccd2879eb908035d4f3a
luiz bueno <luiz504@hotmail.com>
2024-07-29 09:12:07 -0300

    refactor: std supabase import

--- src/services/supabase/middleware.ts
+++ src/services/supabase/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import * as Supabase from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
@@ -6,7 +6,7 @@ export async function updateSession(request: NextRequest) {
     request,
   })
 
-  const supabase = createServerClient(
+  const supabase = Supabase.createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
